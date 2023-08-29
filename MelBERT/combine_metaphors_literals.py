2061060f9e64404bff85f60186747030fdb4549d
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-08-29 14:29:45 -0400

    Resolving Errors

--- MelBERT/combine_metaphors_literals.py
+++ MelBERT/combine_metaphors_literals.py
@@ -3,9 +3,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 columns=["label","sentence","pos","v_index"]
-conventional_df=pd.read_csv("../conventional.tsv",sep="\t",columns=columns)
-novel_df=pd.read_csv("../novel.tsv",sep="\t",columns=columns)
-literals_df=pd.read_csv("../literals.tsv",sep="\t",columns=columns)
+conventional_df=pd.read_csv("../conventional.tsv",sep="\t",names=columns)
+novel_df=pd.read_csv("../novel.tsv",sep="\t",names=columns)
+literals_df=pd.read_csv("../literals.tsv",sep="\t",names=columns)
 
 lit_90, lit_10 = train_test_split(literals_df, test_size=0.1, random_state=42)
 
