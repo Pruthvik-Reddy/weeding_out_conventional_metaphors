2076c8c9c6eab21d233988fbe6ba3c79b699cff5
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-08-28 21:02:32 -0400

    adding python file to combine metaphors with literals and feed to MelBERT

--- MelBERT/combine_metaphors_literals.py
+++ MelBERT/combine_metaphors_literals.py
@@ -3,9 +3,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 columns=["label","sentence","pos","v_index"]
-conventional_df=pd.read_csv("conventional.tsv",sep="\t")
-novel_df=pd.read_csv("novel.tsv",sep="\t")
-literals_df=pd.read_csv("literals.tsv",sep="\t")
+conventional_df=pd.read_csv("../conventional.tsv",sep="\t")
+novel_df=pd.read_csv("../novel.tsv",sep="\t")
+literals_df=pd.read_csv("../literals.tsv",sep="\t")
 
 lit_90, lit_10 = train_test_split(literals_df, test_size=0.1, random_state=42)
 
