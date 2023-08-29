aa550d7b85c92eeb2458d82aa3c3841ab3a50ec7
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-08-29 14:05:13 -0400

    Resolving Errors

--- MelBERT/combine_metaphors_literals.py
+++ MelBERT/combine_metaphors_literals.py
@@ -3,17 +3,17 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 columns=["label","sentence","pos","v_index"]
-conventional_df=pd.read_csv("../conventional.tsv",sep="\t")
-novel_df=pd.read_csv("../novel.tsv",sep="\t")
-literals_df=pd.read_csv("../literals.tsv",sep="\t")
+conventional_df=pd.read_csv("../conventional.tsv",sep="\t",columns=columns)
+novel_df=pd.read_csv("../novel.tsv",sep="\t",columns=columns)
+literals_df=pd.read_csv("../literals.tsv",sep="\t",columns=columns)
 
 lit_90, lit_10 = train_test_split(literals_df, test_size=0.1, random_state=42)
 
 conventional_combined_df = pd.concat([conventional_df, lit_90], ignore_index=True)
 novel_combined_df = pd.concat([novel_df, lit_10], ignore_index=True)
 
-conventional_combined_df.to_csv("conventional_with_literals.tsv",index=True,sep="\t")
-novel_combined_df.to_csv("novel_with_literals.tsv",index=True,sep="\t")
+conventional_combined_df.to_csv("conventional_with_literals.tsv",sep="\t",columns=columns)
+novel_combined_df.to_csv("novel_with_literals.tsv",sep="\t",columns=columns)
 
 
 
